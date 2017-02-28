d1c5b591caa2d8d491a7f8dbe6becf777eae4cd1
zyro <811405258@qq.com>
2017-02-28 20:13:19 +0800

    basic porting for micro2440 from smdk2410

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+
+DEFAULT_PRODUCT_NAME="micro2440"
+
+PRODUCT_NAME=
+
+show_usage()
+{
+cat <<USAGE
+  Usage:
+      $0 <product_name>		;build all for <product_name>
+  Example:
+      $0 micro2440;
+      $0
+USAGE
+}
+
+if [ $# -gt 1 ];then
+	echo "ERROR!"
+	show_usage
+elif [ $# -gt 0 ];then
+	echo "## Start build u-boot for '$1'! ##"
+	PRODUCT_NAME="$1"
+else
+	echo "## Start build u-boot for 'micro2440', enabled by default! ##"
+	PRODUCT_NAME=${DEFAULT_PRODUCT_NAME}
+fi
+
+
+make ${PRODUCT_NAME}_defconfig
+
+make
